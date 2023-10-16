c461b53b9f50e4ed7a703475b83d3e51002d0f21
Student 3 <me@vila.net.br>
2023-10-16 18:14:12 -0300

    Code improvements.

--- iac/modules/stackscript/stackscript.sh
+++ iac/modules/stackscript/stackscript.sh
@@ -7,6 +7,10 @@
 # <UDF name="REMOTEBACKEND_ID" Label="Terraform Remote Backend Identifier" example="Identifier of Terraform Remote Backend used to control the provisioning states of the resources."/>
 # <UDF name="REMOTEBACKEND_REGION" Label="Terraform Remote Backend Region" example="Region of Terraform Remote Backend used to control the provisioning states of the resources."/>
 
+function setHostname() {
+  hostnamectl set-hostname cicdzerotohero
+}
+
 function updateSystem() {
   apt update > /dev/ttyS0
   apt -y upgrade > /dev/ttyS0
@@ -24,6 +28,12 @@ function installRequiredSoftware() {
                  git \
                  unzip \
                  htop > /dev/ttys0
+
+  installTerraform
+  installAkamaiCLI
+  installPowershell
+  installDocker
+  installCiCd
 }
 
 function installTerraform() {
@@ -46,7 +56,11 @@ function installPowershell() {
   tar xvzf powershell-7.3.8-linux-x64.tar.gz > /dev/ttyS0
 }
 
-function installProject() {
+function installDocker() {
+  curl https://get.docker.com | sh - > /dev/ttyS0
+}
+
+function installCiCd() {
   mkdir /root/.aws
 
   git clone https://github.com/fvilarinho/cicdzerotohero /root/cicdzerotohero > /dev/ttyS0
@@ -55,6 +69,17 @@ function installProject() {
 
   rm -rf .git
   rm -f .gitignore
+
+  createEdgeGridFile
+  createRemoteBackend
+}
+
+function createEdgeGridFile() {
+  echo "[default]" > /root/.edgerc
+  echo "host = $EDGEGRID_HOST" >> /root/.edgerc
+  echo "access_token = $EDGEGRID_ACCESS_TOKEN" >> /root/.edgerc
+  echo "client_token = $EDGEGRID_CLIENT_TOKEN" >> /root/.edgerc
+  echo "client_secret = $EDGEGRID_CLIENT_SECRET" >> /root/.edgerc
 }
 
 function createRemoteBackend() {
@@ -86,33 +111,17 @@ function createRemoteBackend() {
             -var "remoteBackendRegion=$REMOTEBACKEND_REGION" > /dev/ttyS0
 }
 
-function createEdgeGridFile() {
-  echo "[default]" > /root/.edgerc
-  echo "host = $EDGEGRID_HOST" >> /root/.edgerc
-  echo "access_token = $EDGEGRID_ACCESS_TOKEN" >> /root/.edgerc
-  echo "client_token = $EDGEGRID_CLIENT_TOKEN" >> /root/.edgerc
-  echo "client_secret = $EDGEGRID_CLIENT_SECRET" >> /root/.edgerc
-}
+function startCiCd() {
+  cd /root/cicdzerotohero || exit 1
 
-function installDocker() {
-  curl https://get.docker.com | sh - > /dev/ttyS0
-}
-
-function setHostname() {
-  hostnamectl set-hostname cicdzerotohero
+  ./start.sh
 }
 
 function main() {
+  setHostname
   updateSystem
   installRequiredSoftware
-  installTerraform
-  installAkamaiCLI
-  installPowershell
-  installDocker
-  installProject
-  createEdgeGridFile
-  createRemoteBackend
-  setHostname
+  startCiCd
 }
 
 main
