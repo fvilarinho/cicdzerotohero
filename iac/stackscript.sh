f9a5ddd239221fd1be6886e9c1f18f44d2477f30
Student 3 <me@vila.net.br>
2023-10-16 15:23:31 -0300

    Code improvements.

--- iac/stackscript.sh
+++ iac/stackscript.sh
@@ -1,43 +1,81 @@
 #!/bin/bash
-# <UDF name="EDGEGRID_HOST" Label="TLS Certificate" example="Copy & Paste the TLS certificate used for HTTPs connection."/>
-# <UDF name="EDGEGRID_ACCESS_TOKEN" Label="TLS Certificate" example="Copy & Paste the TLS certificate used for HTTPs connection."/>
-# <UDF name="EDGEGRID_CLIENT_TOKEN" Label="TLS Certificate" example="Copy & Paste the TLS certificate used for HTTPs connection."/>
-# <UDF name="EDGEGRID_CLIENT_SECRET" Label="TLS Certificate" example="Copy & Paste the TLS certificate used for HTTPs connection."/>
-# <UDF name="ACC_TOKEN" Label="TLS Certificate Key" example="Copy & Paste the TLS certificate key used for HTTPs connection."/>
-# <UDF name="ACC_OBJECT_STORAGE_ACCESS_KEY" Label="TLS Certificate Key" example="Copy & Paste the TLS certificate key used for HTTPs connection."/>
-# <UDF name="ACC_OBJECT_STORAGE_SECRET_KEY" Label="TLS Certificate Key" example="Copy & Paste the TLS certificate key used for HTTPs connection."/>
+# <UDF name="EDGEGRID_HOST" Label="Akamai EdgeGrid Hostname" example="Hostname used to authenticate in Akamai Intelligent Platform using APIs/CLI or Terraform calls."/>
+# <UDF name="EDGEGRID_ACCESS_TOKEN" Label="Akamai EdgeGrid Access Token" example="Access Token used to authenticate in Akamai Intelligent Platform using APIs/CLI or Terraform calls."/>
+# <UDF name="EDGEGRID_CLIENT_TOKEN" Label="Akamai EdgeGrid Client Token" example="Client Token used to authenticate in Akamai Intelligent Platform using APIs/CLI or Terraform calls."/>
+# <UDF name="EDGEGRID_CLIENT_SECRET" Label="Akamai EdgeGrid Client Secret" example="Client Secret used to authenticate in Akamai Intelligent Platform using APIs/CLI or Terraform calls."/>
+# <UDF name="ACC_ACCESS_KEY" Label="Akamai Connected Cloud Access Key" example="Access Key used to store the Terraform State Files in Akamai Connected Cloud Object Storage." default=""/>
+# <UDF name="ACC_SECRET_KEY" Label="Akamai Connected Cloud Secret Key" example="Secret Key used to store the Terraform State Files in Akamai Connected Cloud Object Storage." default=""/>
 
-apt -y update > /dev/ttyS0
-apt -y upgrade > /dev/ttyS0
-apt -y install ca-certificates \
-               curl \
-               wget \
-               dnsutils \
-               net-tools \
-               vim \
-               gnupg2 \
-               sqlite3 \
-               git \
-               unzip \
-               htop > /dev/ttys0
-git clone https://github.com/fvilarinho/cicdzerotohero /root/cicdzerotohero > /dev/ttyS0
-cd /root/cicdzerotohero || exit 1
-mv iac/docker-compose.yml .
-rm -rf .git
-rm -rf iac
-rm -f *.sh
-rm -f *.txt
-rm -f LICENSE
-rm -f *.md
-rm .gitignore
+function updateSystem() {
+  apt update > /dev/ttyS0
+  apt -y upgrade > /dev/ttyS0
+}
 
+function installRequiredSoftware() {
+  apt -y install ca-certificates \
+                 curl \
+                 wget \
+                 dnsutils \
+                 net-tools \
+                 vim \
+                 gnupg2 \
+                 sqlite3 \
+                 git \
+                 unzip \
+                 htop > /dev/ttys0
 
+  wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/ttyS0
 
-echo "$TLS_CERTIFICATE" > ./ingress/settings/cert.crt
-echo "$TLS_CERTIFICATE_KEY" > ./ingress/settings/cert.key
+  echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 
-echo "$EDGE_RC" > /root/.edgerc
+  apt update
+  apt -y install terraform > /dev/ttyS0
+}
 
-echo "$ACC_CREDENTIALS" > /root/.credentials
+function installProject() {
+  mkdir /root/.aws
+  git clone https://github.com/fvilarinho/cicdzerotohero /root/cicdzerotohero > /dev/ttyS0
+  cd /root/cicdzerotohero || exit 1
+  mv iac/docker-compose.yml .
+  rm -rf .git
+  rm -rf iac
+  rm -f *.sh
+  rm -f *.txt
+  rm -f LICENSE
+  rm -f *.md
+  rm .gitignore
+}
 
-curl https://get.docker.com | sh - > /dev/ttyS0
+function createEdgeGridFile() {
+  echo "[default]" > /root/.edgerc
+  echo "host = $EDGEGRID_HOST" >> /root/.edgerc
+  echo "access_token = $EDGEGRID_ACCESS_TOKEN" >> /root/.edgerc
+  echo "client_token = $EDGEGRID_CLIENT_TOKEN" >> /root/.edgerc
+  echo "client_secret = $EDGEGRID_CLIENT_SECRET" >> /root/.edgerc
+}
+
+function createAccCredentials() {
+  echo "[default]" > /root/.aws/credentials
+  echo "aws_access_key_id = $ACC_ACCESS_KEY" >> /root/.aws/credentials
+  echo "aws_secret_access_key = $ACC_SECRET_KEY" >> /root/.aws/credentials
+}
+
+function installDocker() {
+  curl https://get.docker.com | sh - > /dev/ttyS0
+}
+
+function setHostname() {
+  hostnamectl set-hostname cicdzerotohero
+}
+
+function main() {
+  updateSystem
+  installRequiredSoftware
+  installProject
+  createEdgeGridFile
+  createAccCredentials
+  installDocker
+  setHostname
+}
+
+main
