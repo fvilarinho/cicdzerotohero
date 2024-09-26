d51a708035393d1b3bbe4b73fe2553f47b691f66
Felipe Vilarinho <fvilarin@akamai.com>
2024-09-26 14:45:40 -0300

    Code improvements.

--- iac/linode-dns.tf
+++ iac/linode-dns.tf
@@ -4,8 +4,10 @@ data "linode_domain" "default" {
 
 resource "linode_domain_record" "default" {
   domain_id   = data.linode_domain.default.id
+  name        = "gitea.${var.settings.general.domain}"
   record_type = "A"
   target      = linode_instance.default.ip_address
+  ttl_sec     = 30
   depends_on  = [
     data.linode_domain.default,
     linode_instance.default
