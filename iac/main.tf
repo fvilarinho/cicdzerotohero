7ca565a52759033abb2b3a84563ac85b90c6c25a
Felipe Vilarinho <fvilarin@akamai.com>
2024-09-26 23:42:29 -0300

    Update documentation.

--- iac/main.tf
+++ iac/main.tf
@@ -1,5 +1,16 @@
 # Terraform definition.
 terraform {
+  # Stores the provisioning state in Akamai Cloud Computing Object Storage (Please change to use your own).
+  backend "s3" {
+    bucket                      = "fvilarin-devops"
+    key                         = "akamai-as-code.tfstate"
+    region                      = "us-east-1"
+    endpoint                    = "us-east-1.linodeobjects.com"
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+  }
+
   # Required providers definition.
   required_providers {
     linode = {
