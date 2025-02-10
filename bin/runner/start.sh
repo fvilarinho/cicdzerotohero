67da3d175b673ca98f93015aa44903e05454b8dc
Felipe Vilarinho <fvilarin@akamai.com>
2025-02-10 19:34:19 -0300

    Code improvements and Bug fixes.

--- bin/runner/start.sh
+++ bin/runner/start.sh
@@ -9,12 +9,12 @@ docker volume create gitea-runner_data
 # Download the container image from the registry.
 docker pull "$DOCKER_REGISTRY_URL/$DOCKER_REGISTRY_ID/gitea-runner:$BUILD_VERSION"
 
-docker stop gitea-runner
+docker stop gitea-runner 2> /dev/null
 
-docker rm gitea-runner
+docker rm -f gitea-runner 2> /dev/null
 
 # Start the container image.
-docker run --rm \
+docker run \
        -d \
        --privileged \
        --name gitea-runner \
