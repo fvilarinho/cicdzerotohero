3a19dea2e16e87b8261b118581ff5ff65013b2d8
Felipe Vilarinho <fvilarin@akamai.com>
2025-03-03 20:13:03 -0300

    Code improvements and Bug fixes. Adding Akamai artifacts in the deployment. Adding provisioning of Sonarqube.

--- iac/linode-firewall.tf
+++ iac/linode-firewall.tf
@@ -45,6 +45,8 @@ resource "linode_firewall" "server" {
                       [
                         "${linode_instance.runner.ip_address}/32",
                         "${linode_instance.runner.private_ip_address}/32",
+                        "${linode_instance.codeQuality.ip_address}/32",
+                        "${linode_instance.codeQuality.private_ip_address}/32",
                         "${jsondecode(data.http.myIp.response_body).ip}/32"
                       ]
                )
@@ -63,6 +65,7 @@ resource "linode_firewall" "server" {
   depends_on = [
     linode_instance.server,
     linode_instance.runner,
+    linode_instance.codeQuality,
     data.http.myIp
   ]
 }
@@ -108,6 +111,8 @@ resource "linode_firewall" "runner" {
                       [
                         "${linode_instance.server.ip_address}/32",
                         "${linode_instance.server.private_ip_address}/32",
+                        "${linode_instance.codeQuality.ip_address}/32",
+                        "${linode_instance.codeQuality.private_ip_address}/32",
                         "${jsondecode(data.http.myIp.response_body).ip}/32"
                       ]
                )
@@ -126,6 +131,73 @@ resource "linode_firewall" "runner" {
   depends_on = [
     linode_instance.server,
     linode_instance.runner,
+    linode_instance.codeQuality,
+    data.http.myIp
+  ]
+}
+
+# Firewall rules to protect Sonarqube server.
+resource "linode_firewall" "codeQuality" {
+  label           = "${var.settings.codeQuality.name}-fw"
+  tags            = var.settings.codeQuality.tags
+  inbound_policy  = "DROP"
+  outbound_policy = "ACCEPT"
+
+  inbound {
+    action   = "ACCEPT"
+    label    = "allow-akamai-ips"
+    protocol = "TCP"
+    ports    = "22,443"
+    ipv4 = [
+      "139.144.212.168/31",
+      "172.232.23.164/31",
+      "172.236.119.4/30",
+      "172.234.160.4/30",
+      "172.236.94.4/30"
+    ]
+    ipv6 = [
+      "2600:3c06::f03c:94ff:febe:162f/128",
+      "2600:3c06::f03c:94ff:febe:16ff/128",
+      "2600:3c06::f03c:94ff:febe:16c5/128",
+      "2600:3c07::f03c:94ff:febe:16e6/128",
+      "2600:3c07::f03c:94ff:febe:168c/128",
+      "2600:3c07::f03c:94ff:febe:16de/128",
+      "2600:3c08::f03c:94ff:febe:16e9/128",
+      "2600:3c08::f03c:94ff:febe:1655/128",
+      "2600:3c08::f03c:94ff:febe:16fd/128"
+    ]
+  }
+
+  inbound {
+    action   = "ACCEPT"
+    label    = "allow-external-ips"
+    protocol = "TCP"
+    ports    = "22,2222,80,443"
+    ipv4     = concat(var.settings.codeQuality.allowedIps.ipv4,
+      [
+        "${linode_instance.server.ip_address}/32",
+        "${linode_instance.server.private_ip_address}/32",
+        "${linode_instance.runner.ip_address}/32",
+        "${linode_instance.runner.private_ip_address}/32",
+        "${jsondecode(data.http.myIp.response_body).ip}/32"
+      ]
+    )
+    ipv6     = concat(var.settings.codeQuality.allowedIps.ipv6, [ "::1/128" ])
+  }
+
+  inbound {
+    action   = "ACCEPT"
+    label    = "allow-icmp"
+    protocol = "ICMP"
+    ipv4     = [ "0.0.0.0/0" ]
+  }
+
+  linodes = [ linode_instance.codeQuality.id ]
+
+  depends_on = [
+    linode_instance.server,
+    linode_instance.runner,
+    linode_instance.codeQuality,
     data.http.myIp
   ]
 }
